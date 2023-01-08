c7df84c4c43c3897cc98a0c31e6c7821cb141274
Alexander <mailtoalexche@gmail.com>
2023-01-08 03:48:06 +0300

    task

---
+++ api_with_restrictions/advertisements/views.py
@@ -0,0 +1,24 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+from advertisements.models import Advertisement
+from advertisements.serializers import AdvertisementSerializer
+from advertisements.permissions import IsOwnerOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
+from advertisements.models import Date_filter
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['status', 'created_at']
+    filterset_class = Date_filter
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            return [IsAuthenticated(), IsOwnerOrReadOnly()]
+        return []
